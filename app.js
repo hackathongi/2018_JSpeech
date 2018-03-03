e5a62e067bf87696338ac71ea4df1e109b0b0269
Jonathan Millan Trujillo <jmillantr@gmail.com>
2018-03-03 15:02:14 +0100

    Initial

---
+++ app.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var app = express();
+
+app.set('views', './views');
+app.set('view engine', 'ejs');
+app.use('/static', express.static('static'));
+
+app.get('/', function (req, res) {
+    res.render('index');
+});
+
+app.listen(8080, function () {
+    console.log('Example app listening on port 3000!');
+});
